024317bb55eb27e542c8e1da2f60a6cab0128b9e
scaldingblood@gmail.com <scaldingblood@gmail.com>
2020-04-30 11:29:49 +0800

    fix bug exp handler

--- crawler/spider.py
+++ crawler/spider.py
@@ -10,6 +10,9 @@ class Spider:
         self.ck = ck
         self.dbcl = dbcl
         self.session = requests.Session()
+        self.headers = {
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36',
+        }
 
     # def login(self, name, pwd):
     #     name = name.replace("@", "%40")
@@ -38,36 +41,36 @@ class Spider:
     def comment(self, url, content):
         img_url, img_id = self.get_verify_code_img(url)
 
-        pic_path = util.save_pic_to_disk(img_url)
-        verify_code = verify.get_word_in_pic(pic_path)
+        verify_code = ""
+        if len(img_url) > 0:
+            pic_path = util.save_pic_to_disk(img_url)
+            verify_code = verify.get_word_in_pic(pic_path)
 
         comment_url = url + "add_comment"
         self.session.cookies.set_cookie(requests.cookies.create_cookie(domain='.douban.com', name='dbcl2',
                                                             value=self.dbcl))
         self.session.cookies.set_cookie(requests.cookies.create_cookie(domain='.domain.com', name='ck',
                                                             value=self.ck))
-        headers = {
-            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36',
-        }
         data = {"ck": self.ck, "rv_comment": content, "submit_btn": "发送", "start": 0, "captcha-solution": verify_code, "captcha-id": img_id}
-        resp = self.session.post(comment_url, headers=headers, data=data)
+        resp = self.session.post(comment_url, headers=self.headers, data=data)
         print(resp.text)
         print(resp)
         return url
 
     def get_verify_code_img(self, url):
-        resp = self.session.get(url)
+        resp = self.session.get(url, headers=self.headers)
         soup = BeautifulSoup(resp.text, "lxml")
-        img_url = soup.select(".captcha_image")[0]['src']
-        pic_id = soup.select("input[name=captcha-id]")[0]['value']
+        img_url = ""
+        pic_id = ""
+        try:
+            img_url = soup.select(".captcha_image")[0]['src']
+            pic_id = soup.select("input[name=captcha-id]")[0]['value']
+        except:
+            print("no verify img")
         return img_url, pic_id
 
-    @staticmethod
-    def check_posts():
-        headers = {
-            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36',
-        }
-        resp = requests.get(define.GROUP_URL, headers=headers)
+    def check_posts(self):
+        resp = requests.get(define.GROUP_URL, headers=self.headers)
         soup = BeautifulSoup(resp.text, "lxml")
         items = soup.select(".olt tr")
 
